78dceae2dac678fedf728a7569a04e56530f0920
lucascarril <lucasezequiel200405@gmail.com>
2023-04-17 21:22:33 -0300

    primera pre entrega

---
+++ primeraPracticaJS.js
@@ -0,0 +1,6 @@
+let numero = prompt("Ingrese un número:");
+if (numero > 10) {
+  console.log("El número ingresado es mayor que 10");
+} else {
+  console.log("El número ingresado es menor o igual a 10");
+}
